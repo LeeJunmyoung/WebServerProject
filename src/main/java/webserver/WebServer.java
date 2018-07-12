f47dec360757f1c28ec5f49845470085141e427d
LEEJM <LEEJM@LEEJM-PC>
2018-07-12 13:27:42 +0900

    commit

---
+++ src/main/java/webserver/WebServer.java
@@ -0,0 +1,17 @@
+package webserver;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WebServer {
+	private static final Logger log = LoggerFactory.getLogger(WebServer.class);
+
+	public static void main(String args[]) throws Exception {
+		
+		log.info("HELLO WEB SERVER");
+		
+	}
+}
