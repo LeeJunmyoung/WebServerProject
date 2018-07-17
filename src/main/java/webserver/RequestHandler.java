95d8e39cad439aa55592297969461245b5c40166
LEEJM <LEEJM@LEEJM-PC>
2018-07-17 11:25:50 +0900

    html 가져오기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -28,30 +28,62 @@ public class RequestHandler extends Thread{
 		
 		log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),connection.getPort());
 		
+		
+		
 		try(InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
 			
 			BufferedReader br = new BufferedReader( new InputStreamReader(in,"UTF-8") );
 			
-			String firstLine = br.readLine();
-			log.info(firstLine);  
+			String firstLine      = br.readLine();
+			String arrFirstLine[] = firstLine.split(" ");
+			if(arrFirstLine.length==3){
+			String httpMethod     = arrFirstLine[0];
+			String httpUrl        = arrFirstLine[1].equals("/")?"/index.html":arrFirstLine[1];
+			String httpProtocol   = arrFirstLine[2];
+			
+			log.info("Http Method   : {}",httpMethod);
+			log.info("Http Url      : {}",httpUrl);
+			log.info("Http Protocol : {}",httpProtocol);
+			
+			
+			
 			
 			Map<String,String> headerMap = RequestUtill.readHeader(br);
 			
-			log.info("Content-Length : {}",Integer.valueOf(headerMap.get("Content-Length")));
-			
-			String requestBody=RequestUtill.readData(br,Integer.valueOf(headerMap.get("Content-Length")));
-			log.info(requestBody.trim());                  
+			if(!httpMethod.equals("GET")){
+				log.info("Content-Length : {}",Integer.valueOf(headerMap.get("Content-Length")));
+				String requestBody=RequestUtill.readData(br,Integer.valueOf(headerMap.get("Content-Length")));
+				log.info(requestBody.trim());                  
+			}
 			
 			DataOutputStream dos = new DataOutputStream(out);
-			byte[] body = "hello".getBytes();
+			
+			File file = new File("./webapp"+httpUrl);
+			byte[] body;
+			if(file.exists()){
+				body = Files.readAllBytes(file.toPath());
+			}else{
+				body = Files.readAllBytes(new File("./webapp/404page.html").toPath());
+			}
+			
             
 			ResponseUtill.response200Header(dos, body.length);
 			ResponseUtill.responseBody(dos, body);
-			
+			}else{
+				throw new Exception("INVALID FORMAT");
+			}
 			
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			log.error("error:"+e);
+		} catch (Exception e){
+			log.error("error:"+e);
+		} finally {
+			try {
+				connection.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				log.error("error:"+e);
+			}
 		}
 	}
 	
